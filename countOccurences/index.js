4e1ff364f9562b4f65f32c2fcde2d28c66278e59
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2018-09-25 14:07:34 -0400

    passed countOccurences tests

--- countOccurences/index.js
+++ countOccurences/index.js
@@ -1,3 +1,34 @@
 module.exports = function countOccurences(tree) {
+  const node = JSON.parse(tree);
+  let newArr = [];
+  let newObj = {};
 
+  function accessObj(obj) {
+    for (let key of Object.keys(obj)) {
+      let value = obj[key];
+      if (typeof value === 'object') {
+        accessObj(value);
+      } else {
+        if (newObj.hasOwnProperty(key)) {
+          newObj[key] += value;
+        } else {
+          newObj[key] = value;
+        }
+      }
+    }
+  }
+
+
+  accessObj(node);
+  let max = Math.max.apply(null, Object.keys(newObj).map(function(x) {
+    return newObj[x];
+  }));
+
+  newArr.push(max);
+
+  newArr.push(Object.keys(newObj).filter(function(x) {
+    return newObj[x] == max;
+  }));
+
+  return newArr;
 };
